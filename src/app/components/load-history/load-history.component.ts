ab26928a1fafe17ec018383ffd8adcac0b9136bd
Steve Pak <stevedpak@gmail.com>
2019-03-30 23:41:45 -0400

    added time-series for load history.

--- src/app/components/load-history/load-history.component.ts
+++ src/app/components/load-history/load-history.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServerLoadService } from 'src/app/services/server-load.service';
+import { D3MultiData } from 'src/app/model/d3-multi-data';
 
 @Component({
   selector: 'app-load-history',
@@ -7,16 +8,40 @@ import { ServerLoadService } from 'src/app/services/server-load.service';
   styleUrls: ['./load-history.component.css']
 })
 export class LoadHistoryComponent implements OnInit {
-  cpuLoads: number[] = [];
+
+  data: D3MultiData[] = [
+    {
+      name: 'CPU Load',
+      series: []
+    }
+  ];
+
+  colorScheme = {
+    domain: ['green']
+  };
+
+  xAxis() {
+    if (this.data[0].series.length > 30) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
 
   constructor(private serverLoadService: ServerLoadService) { }
 
   ngOnInit() {
     this.serverLoadService.getLoadAverage().subscribe(load => {
-      this.cpuLoads.push(load);
-      if (this.cpuLoads.length > 10) {
-        this.cpuLoads.shift();
+      const today = new Date();
+      const time = today.getHours() + ":" + today.getMinutes().toString().padStart(2, '0') + ":" + today.getSeconds().toString().padStart(2, '0');
+      let series = this.data[0].series;
+      series.push({ name: time, value: load });
+      if (series.length > 60) {
+        series.shift();
       }
+      this.data[0].series = series;
+      this.data = [...this.data];
     });
   }
 
---
+++ src/app/model/d3-multi-data.ts
@@ -0,0 +1,6 @@
+import { D3Data } from './d3-data';
+
+export class D3MultiData {
+    name: string;
+    series: D3Data[];
+}
